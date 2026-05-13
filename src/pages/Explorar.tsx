858bfd086a78ed9700502c0955cb348278e17c67
lucashirotsu <hirotsulucas@hotmail.com>
2026-05-13 14:12:49 -0300

    Alterações visuais na página Explorar

--- src/pages/Explorar.tsx
+++ src/pages/Explorar.tsx
@@ -29,63 +29,41 @@ export default function Explorar() {
 
     return (
         <>
-            <div className="paddingHeader" id='paddingImgFade'></div>
-
-            <section className='img-fade' id='capivara'>
-
-                <div className="conteudo vertical">
-
-                    <div className="info vertical gap5">
-                        <h1>Todas as Atividades</h1>
-
-                        <p>
-                            Descubrar caminhos, fauna e paisagens únicas
-                            às margens do Rio Juqueriquerê
-                        </p>
-                    </div>
-
-                    <h1>Trilhas</h1>
-
-                    <div className='carrossel horizontal' id='filtros'>
-                        <div className="horizontal gap5">
-                            {trilhasList}
-                        </div>
-                    </div>
-
-                    <div className="lista vertical">
-
-                        <h1>Pontos de interesse</h1>
-
-                        <div className="horizontal gap5">
-
-                            {trilhas.map((trilha) => (
-
-                                <button
-                                    key={trilha.id}
-                                    onClick={() => setTrilhaSelecionada(trilha)}
-                                    className={
-                                        trilhaSelecionada.id === trilha.id
-                                            ? 'ativo'
-                                            : ''
-                                    }
-                                >
-                                    {trilha.nome}
-                                </button>
-
-                            ))}
-
-                        </div>
-
-                        <div className="vertical gap5">
-
-                            {pontosList}
-
-                        </div>
-
-                    </div>
-
+            <div className="paddingHeader"></div>
+            <section className="conteudo vertical">
+                
+                <div className="mapa">
+                    <p>O mapa vai aqui</p>
                 </div>
 
+                <div className="vertical gap5">
+                    <h1>Trilhas</h1>
+                    <div className='carrossel horizontal'>
+                        {trilhasList}
+                    </div>
+                </div>
+
+                <div className="lista vertical">
+                    <h1>Pontos de interesse</h1>
+                    <div className="horizontal gap5">
+                        {trilhas.map((trilha) => (
+                            <button
+                                key={trilha.id}
+                                onClick={() => setTrilhaSelecionada(trilha)}
+                                className={
+                                    trilhaSelecionada.id === trilha.id
+                                        ? 'ativo'
+                                        : ''
+                                }
+                            >
+                                {trilha.nome}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="vertical gap5">
+                        {pontosList}
+                    </div>
+                </div>
             </section>
         </>
     );
