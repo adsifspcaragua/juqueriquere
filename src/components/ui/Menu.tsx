11b8b0ce82e27aa6a7bb62be21501170fc7a6919
lucashirotsu <hirotsulucas@hotmail.com>
2026-06-22 23:52:22 -0300

    Alteração visual no Menu

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -3,7 +3,6 @@ import type Trilha from '../../pages/Trilhas/TrilhaInfo.tsx';
 import SimpleButton from '../../components/ui/buttons/SimpleButton.tsx';
 import { useState } from 'react';
 import './Menu.css';
-import { icons } from './icons.tsx';
 
 interface menuProps {
     ativo: boolean;
@@ -11,27 +10,19 @@ interface menuProps {
 }
 
 export default function Menu({ ativo, onChoice }: menuProps) {
-    const { Explorar, Sobre, Home } = icons.default;
 
     const trilhas: Trilha[] = [...data.trilhas] as Trilha[]; // Asserção de tipo para garantir que temos um array de Trilha
+    const pontos = trilhas
+        .flatMap((trilha) =>
+            trilha.pontos_interesse.map((ponto) => ({
+                ...ponto,
+                trilhaId: trilha.id
+            }))
+        )
+        .slice(0, 5);   
+
     const [trilhasShow, setTrilhasShow] = useState(false);
     const [pontosShow, setPontosShow] = useState(false);
-
-    const buttonMenu = (path?: string, title?: string, icon?: string, dark?: boolean) => {
-        return (
-            <div className={`MenuGroup ${dark ? 'dark' : ''}`}>
-                <SimpleButton
-                    tema='dark'
-                    icon="none"
-                    raio="0"
-                    path={`/${path}`}
-                >
-                    <h3>{title}</h3>
-                    <img src={icon}/>
-                </SimpleButton>
-            </div>
-        );
-    }
 
     return (
         <div
@@ -43,81 +34,82 @@ export default function Menu({ ativo, onChoice }: menuProps) {
                 onClick={(e) => e.stopPropagation()}
             >
 
-                <div className="menuLista vertical gap5">
+                <div className="menuLista vertical gap15">
 
                     <h1>Menu</h1>
 
-                    {/*fazer renderização condicional dps, apenas para web*/}
-                    {buttonMenu("", "Início", Home, true)}
-                    {buttonMenu("sobre", "Sobre", Sobre, true)}
-                    {buttonMenu("explorar", "Mapa", Explorar, true)}
-                    
-                    {/* TRILHAS */}
-                    <div className='MenuGroup'>
-
-                        <SimpleButton
-                            icon="none"
-                            raio="0"
-                            onClick={() => setTrilhasShow(!trilhasShow)}
-                        >
-                            <h3>Trilhas</h3>
-                            <h3>{trilhasShow ? '-' : '+'}</h3>
-                        </SimpleButton>
-
-                        <div className={`children ${trilhasShow ? 'open' : ''}`}>
-
-                            {trilhas.map((trilha) => (
-                                <SimpleButton
-                                    key={trilha.nome + "option"}
-                                    tema='dark'
-                                    raio="0"
-                                    path={`/trilha/${trilha.id}`}
-                                    onClick={onChoice}
-                                >
-                                    {trilha.nome}
-                                </SimpleButton>
-                            ))}
-                            <SimpleButton
-                                raio="0"
-                                path='/explorar'
-                            >
-                                <h3>Ver todas as trilhas</h3>
-                            </SimpleButton>
-                        </div>
+                    <div className="menuLinks">
+                        <SimpleButton path='/' raio='0'>Início</SimpleButton>
+                        <SimpleButton path='/explorar' raio='0'>Mapa</SimpleButton>
+                        <SimpleButton path='/sobre' raio='0'>Sobre</SimpleButton>
                     </div>
 
-                    {/* PONTOS */}
-                    <div className='MenuGroup'>
-                        <SimpleButton
-                            icon="none"
-                            raio="0"
-                            onClick={() => setPontosShow(!pontosShow)}
-                        >
-                            <h3>Pontos</h3>
-                            <h3>{pontosShow ? '-' : '+'}</h3>
-                        </SimpleButton>
+                    <div className="menuLinks">
+                        {/* TRILHAS */}
+                        <div className='MenuGroup'>
+                            <SimpleButton
+                                icon="none"
+                                raio="0"
+                                onClick={() => setTrilhasShow(!trilhasShow)}
+                            >
+                                Trilhas
+                                <div className={`arrow ${trilhasShow ? 'open' : ''}`}></div>
+                            </SimpleButton>
 
-                        <div className={`children ${pontosShow ? 'open' : ''}`}>
-
-                            {trilhas.map((trilha) =>
-                                trilha.pontos_interesse.map((ponto, index) => (
+                            <div className={`children ${trilhasShow ? 'open' : ''}`}>
+                                {trilhas.slice(0, 5).map((trilha) => (
                                     <SimpleButton
-                                        key={`${trilha.id}-${index}`}
-                                        icon='none'
-                                        tema='dark'
+                                        key={trilha.nome + "option"}
                                         raio="0"
+                                        path={`/trilha/${trilha.id}`}
+                                        onClick={onChoice}
+                                    >
+                                        {trilha.nome}
+                                    </SimpleButton>
+                                ))}
+                                <SimpleButton
+                                    raio="0"
+                                    path='/trilhas'
+                                >
+                                    <b>Ver todas as trilhas</b>
+                                </SimpleButton>
+                            </div>
+                        </div>
+
+                        {/* PONTOS */}
+                        <div className='MenuGroup'>
+                            <SimpleButton
+                                icon="none"
+                                raio="0"
+                                onClick={() => setPontosShow(!pontosShow)}
+                            >
+                                Pontos
+                                <div className={`arrow ${pontosShow ? 'open' : ''}`}></div>
+                            </SimpleButton>
+                            <div className={`children ${pontosShow ? 'open' : ''}`}>
+                                {pontos.map((ponto, index) => (
+                                    <SimpleButton
+                                        key={`${ponto.nome}-${index}`}
+                                        raio="0"
+                                        path={`/trilha/${ponto.trilhaId}/ponto/${encodeURIComponent(ponto.nome)}`}
                                     >
                                         {ponto.nome}
                                     </SimpleButton>
-                                ))
-                            )}
-                            
-
+                                ))}
+                                <SimpleButton
+                                    raio="0"
+                                    path='/Pontos'
+                                >
+                                    <b>Ver todas os pontos</b>
+                                </SimpleButton>
+                            </div>
                         </div>
                     </div>
                     
                     {/* Acesso Temporário */}
-                    <SimpleButton path='/admin' raio='10'>Administração do Site</SimpleButton>
+                    <div className="menuLinks">
+                        <SimpleButton path='/admin/login' raio='0'>Administração do Site</SimpleButton>
+                    </div>
                 </div>
             </div>
         </div>
