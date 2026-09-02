1bcea583f4f69e3d1d39c501979e64f4bc68fa08
Lucas Hirotsu <hirotsulucas@hotmail.com>
2026-09-02 20:38:04 -0300

    Alteração Visual no footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,10 +1,36 @@
 import meioAmbiente from "../../assets/meioAmbiente.webp";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.webp"
 import '../styles/Footer.css';
 
 export default function Footer(){
     return (
         <footer className="vertical">
-            <img src={meioAmbiente} alt="Meio ambiente" />
+            <div className="conteudoFooter gap30">
+                <div className="footerLogos horizontal gap30">
+                    <img src={meioAmbiente} alt="Meio ambiente" />
+                    <div className="linhaVertical" />
+                    <div className="linhaHorizontal" />
+                    <img src={logo} alt="Parque Natural Municipal Juqueriquerê" />
+                </div>
+
+                <div className="horizontal gap30" id="linksFooter">
+                    <div className="vertical gap5">
+                        <Link to={"/"}>Início</Link>
+                        <Link to={"/explorar"}>Mapa</Link>
+                        <Link to={"/sobre"}>Sobre o parque</Link>
+                    </div>
+                    <div className="vertical gap5">
+                        <Link to={"/trilhas"}>Trilhas</Link>
+                        <Link to={"/pontos"}>Pontos de Interesse</Link>
+                    </div>
+                    <div className="vertical gap5">
+                        <Link to={"/"}>Termos de Uso</Link>
+                        <Link to={"/"}>Política de Privacidade</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="linhaHorizontal"></div>
             <p>© 2026 - Parque Natural Municipal Juqueriquerê</p>
         </footer>
     );
